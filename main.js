50e79f09b89f954aaa5fb437041649212a7f6c77
Aman Dangol <amandangol423@gmail.com>
2023-12-04 19:27:35 +0545

    word check added

--- main.js
+++ main.js
@@ -1,8 +1,9 @@
 import { countries } from "./countryName.js";
 let blank = document.querySelector(".blank");
-let inputBox = document.getElementById('input');
+let inputBox = document.getElementById("input");
 let guessWord;
 let guessWordLength;
+let userWord;
 
 //defining functions
 function getword() {
@@ -12,18 +13,37 @@ function getword() {
   guessWordLength = guessWord.length;
   console.log(guessWord + " " + guessWordLength);
 }
+
 function getRandomNumer(max) {
   return Math.floor(Math.random() * max);
 }
+
 function createBlanks() {
   for (let i = 0; i < guessWordLength; i++) {
     if (guessWord[i] == " ") {
       console.log("space");
       blank.innerHTML += "  ";
     } else {
-      blank.innerHTML += "_  ";
+      blank.innerHTML += "_ ";
     }
   }
+}
+function show(){
+    blank.innerHTML= guessWord;
+}
+
+
+function checkWord() {
+  if (userWord.toLowerCase() == guessWord.toLowerCase()) {
+    show()
+    console.log("you won!!");
+  }
+  // for (let i = 0; i < guessWordLength; i++) {
+  //     for (let j = 0; j < userWord.length; j++) {
+  //         if(i==j){
+  //             console.log(userWord[j]);
+  //         }
+  //     }
 }
 
 //calling function
@@ -31,8 +51,9 @@ getword();
 createBlanks();
 
 //event handler
-inputBox.addEventListener('keydown',(e)=>{
-    if(e.key == "Enter"){
-        
-    }    
-})
+inputBox.addEventListener("keydown", (e) => {
+  if (e.key == "Enter") {
+    userWord = inputBox.value;
+    checkWord();
+  }
+});
