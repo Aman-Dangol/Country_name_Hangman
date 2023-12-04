f8af0372644d2b477a93b7a874d8793b07911798
Aman Dangol <amandangol423@gmail.com>
2023-12-04 18:18:47 +0545

    used array instead of require('fs)

--- main.js
+++ main.js
@@ -1,11 +1,2 @@
-const { log } = require("console");
-const fs = require("fs");
-const fileName = "countryNames.txt";
-let countries;//array for countries
+import { countries } from "./countryName.js";
 
-function readFile() {
-  const data = fs.readFileSync(fileName).toString();
-  countries = data.split("\r");
-  console.log(countries);
-}
-readFile();
